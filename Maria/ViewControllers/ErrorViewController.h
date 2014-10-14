765e7eaa934317931980486d00d2db2c8fbfbb24
Juan Francisco <escuderojuan@gmail.com>
2014-10-14 19:11:47 -0300

    Add error management.

---
+++ Maria/ViewControllers/ErrorViewController.h
@@ -0,0 +1,13 @@
+//
+//  ErrorViewController.h
+//  Maria
+//
+//  Created by Juan Francisco on 11/10/14.
+//  Copyright (c) 2014 Juan Escudero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ErrorViewController : UIViewController
+
+@end
