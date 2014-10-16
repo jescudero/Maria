0750eb05b91a77e4eccccce015fdbecc54a74df2
Juan Escudero <escuderojuan@gmail.com>
2014-10-16 18:10:03 -0300

    Add log storyboard.

---
+++ Maria/ViewControllers/CultivosLogTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CultivosLogTableViewController.h
+//  Maria
+//
+//  Created by Juan Escudero on 10/16/14.
+//  Copyright (c) 2014 Juan Escudero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CultivosLogTableViewController : UITableViewController
+
+@end
